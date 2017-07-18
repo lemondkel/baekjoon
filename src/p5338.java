f0960119e50807578758605fbe2e1c6285467fc9
이두종 <l2jong@insedu.net>
2017-07-18 17:58:38 +0900

    마이크로소프트 로고 5338번

---
+++ src/p5338.java
@@ -0,0 +1,14 @@
+public class p5338 {
+	/**
+	 * l2jong insedu (2017-07-18)
+	 */
+
+	public static void main(String[] args) {
+
+		System.out.println("       _.-;;-._");
+		System.out.println("\'-..-\'|   ||   |");
+		System.out.println("\'-..-\'|_.-;;-._|");
+		System.out.println("\'-..-\'|   ||   |");
+		System.out.println("\'-..-\'|_.-\'\'-._|");
+	}
+}
