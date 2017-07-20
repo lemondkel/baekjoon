c015e8d9144c82bc7404f9d03c33be7f82f35fef
이두종 <l2jong@insedu.net>
2017-07-20 13:53:59 +0900

    웰컴 5337번

---
+++ src/p5337.java
@@ -0,0 +1,12 @@
+public class p5337 {
+	/**
+	 * l2jong insedu (2017-07-20)
+	 */
+
+	public static void main(String[] args) {
+
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
